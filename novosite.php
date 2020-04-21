0e277e8857e7c8fa40ac9615fa05de36a36dac3b
paulo luiz da silva jr <compatek@gmail.com>
2020-04-21 03:20:00 -0300

    trabalhando no arquivo de criação dos arquivos dos sites (acrescentar linha no hosts)

--- novosite.php
+++ novosite.php
@@ -1,6 +1,4 @@
 <?php
-
-
 /*=======================================================================================/
     Funções genericas                                                                    /  
 /=======================================================================================*/
@@ -11,8 +9,8 @@ function lerArquivo($caminho){
         $linha = fgets($arquivo, 1024);//Mostra uma linha do arquivo
         $texto .= $linha; //Monta o conteudo recebido linha por linha
     }
-    return $texto;//retorna o conteudo do arquivo
     fclose($arquivo);// Fecha arquivo aberto
+    return $texto;//retorna o conteudo do arquivo
 }
 
 function gravarArquivo($caminho, $conteudo, $permisao = false){
@@ -23,7 +21,9 @@ function gravarArquivo($caminho, $conteudo, $permisao = false){
     $arquivo = fopen($caminho, "w");
     fwrite($arquivo, $conteudo);
     fclose($arquivo);// Fecha arquivo aberto
-    if($permisao==true){ echo shell_exec("sudo chmod 744 {$caminho}");}
+    if($permisao==true){ 
+        shell_exec("sudo chmod 744 {$caminho}");
+    }
     
 }
 
@@ -32,22 +32,39 @@ function gravarArquivo($caminho, $conteudo, $permisao = false){
     Funções da Aplicação                                                                 /  
 /=======================================================================================*/
 
-function acrecentarLinhatexto($texto){
+function acrecentarLinhatexto($texto,$linha,$valor=""){
     //,$linha,$valor
-    var_dump(explode("\n", $texto));
-   
-   /* if(mb_strpos($texto, $valor)) {
-        $texto .= $linha;
-        $texto .= "192.168.0.4	www.teste.local\n"; 
-    }else{ 
-        $texto .= $linha; 
-    }*/
+    $nv_texto = "";
+    if($valor!=""){ $valor = "{$valor} \n"; }
+
+    $lista = explode(" \n", $texto);
+
+    foreach ($lista as $nv_linha) { //roda um loop das linha encontradas no $texto
+
+        //verifica a linha do texto para saber se é igual ao parametro $linha passado 
+        // ou se a linha do texto é igual ao parametro $valor
+        if($nv_linha==$linha or $nv_linha." \n"==$valor) { //caso alguma das verificações acima for verdadeira rodas o bloco se não roda o else
+            
+            //verifica se a linha é diferente do parametro valor 
+            if($nv_linha." \n"!=$valor){ //caso seja roda o bloco se não pula o bloco
+
+                $nv_texto .= "{$nv_linha} \n";//adiciona a linha do texto
+                $nv_texto .= $valor;//adiciona o parametro $valor numa nova linha
+
+            }
+
+        }else{ 
+
+            $nv_texto .= "{$nv_linha} \n"; //adiciona a linha do texto
+
+        }
+    }
+    return $nv_texto;
 }
 
 /*_______________________________________________________________________________________/
     Aplicação                                                                            /
 /_______________________________________________________________________________________*/
 $texto = lerArquivo("/etc/hosts");
-acrecentarLinhatexto($texto);
-echo $texto;
-//gravarArquivo("/etc/hosts", $conteudo, true);
+$conteudo = acrecentarLinhatexto($texto,"127.0.1.1 rooter","{$_SERVER['REMOTE_ADDR']} www.texte.local");
+gravarArquivo("/etc/hosts", $conteudo, true);
